35d1400a72941455fa6cf45896b39821d75750f7
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-11-14 21:02:30 -0300

    feat: added new screen

--- src/ast/core_ast.rs
+++ src/ast/core_ast.rs
@@ -319,6 +319,8 @@ lazy_static! {
         media_query_variables.insert("minPortraitTablets".to_string(), vec![]);
         media_query_variables.insert("minStandardDesktops".to_string(), vec![]);
         media_query_variables.insert("minLargeDesktops".to_string(), vec![]);
+        media_query_variables.insert("minExtraLargeDesktops".to_string(), vec![]);
+        media_query_variables.insert("maxExtraLargeDesktops".to_string(), vec![]);
         media_query_variables.insert("maxLargeDesktops".to_string(), vec![]);
         media_query_variables.insert("maxStandardDesktops".to_string(), vec![]);
         media_query_variables.insert("maxPortraitTablets".to_string(), vec![]);
