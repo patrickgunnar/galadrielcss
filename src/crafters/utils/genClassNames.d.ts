eb40754971fe3bab7ed86d768724bc9586c10b62
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-11-29 12:00:23 -0300

    refactor: removed content

--- src/crafters/utils/genClassNames.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generate class names from a record of classes.
- *
- * @param classes - The record of classes, where keys are class names, and values can be strings or objects.
- * @returns The generated class names as a string.
- */
-export function genClassNames(classes: Record<string, any>): string;
